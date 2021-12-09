24fb2e449ce20714798f3d92380e1711ca058c39
Chun-Yu Sheu <chun-yu.sheu@outlook.de>
2021-12-09 21:45:05 +0100

    add day6

--- day6/lanternfish.py
+++ day6/lanternfish.py
@@ -20,6 +20,23 @@ def lanternfish_amount_after_n_days(list_of_lanternfish_ages, n):
     return len(list_of_lanternfish_ages)
 
 
+def lanternfish_amount_after_n_days_dict_solution(list_of_lanternfish_ages, n):
+    lanternfish_age_to_amount_dict = {}
+    for age in range(8+1):
+        lanternfish_age_to_amount_dict[age] = list_of_lanternfish_ages.count(age)
+
+    for i in range(n):
+        for age in range(8+1):
+            if age == 0:
+                amount_of_age_0 = lanternfish_age_to_amount_dict[age]
+            else:
+                lanternfish_age_to_amount_dict[age-1] = lanternfish_age_to_amount_dict[age]
+        lanternfish_age_to_amount_dict[6] += amount_of_age_0
+        lanternfish_age_to_amount_dict[8] = amount_of_age_0
+        logger.debug(f"After {i+1} day(s): {lanternfish_age_to_amount_dict.values()}")
+    return sum(lanternfish_age_to_amount_dict.values())
+
+
 def main():
     logging.basicConfig(
         format="%(asctime)s | %(levelname)-7s | %(message)s",
@@ -32,8 +49,8 @@ def main():
     # lanternfish_amount_after_80_days = lanternfish_amount_after_n_days(list_of_lanternfish_ages, 80)
     # logger.info(f"Part 1 answer is '{lanternfish_amount_after_80_days}'")  # 394994
 
-    lanternfish_amount_after_256_days = lanternfish_amount_after_n_days(list_of_lanternfish_ages, 256)
-    logger.info(f"Part 2 answer is '{lanternfish_amount_after_256_days}'")  #
+    lanternfish_amount_after_256_days = lanternfish_amount_after_n_days_dict_solution(list_of_lanternfish_ages, 256)
+    logger.info(f"Part 2 answer is '{lanternfish_amount_after_256_days}'")  # 1765974267455
 
 
 if __name__ == "__main__":
